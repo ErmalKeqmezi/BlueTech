a4c96e089df699a557aba4b2843f29fcac25c666
ArtinTopojani <artintopojani2002@gmail.com>
2023-08-19 01:54:54 +0200

    Components

--- client/src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1>BlueTech</h1>;
-}
-
-export default App;
---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { Product } from "../models/product";
+import Catalog from "../../features/catalog/Catalog";
+
+function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  function addProduct() {
+    setProducts((prevState) => [
+      ...prevState,
+      {
+        id: prevState.length + 101,
+        name: "prod" + (prevState.length + 1),
+        price: prevState.length * 100 + 100,
+        brand: "brand",
+        description: "description ads",
+        // pictureUrl: "http://photo1.jpg",
+      },
+    ]);
+  }
+
+  return (
+    <div>
+      <h1>BlueTech</h1>
+      <Catalog products={products} addProduct={addProduct} />
+    </div>
+  );
+}
+
+export default App;

6fa3d8a3787aaafdb85e0129c920555eeb2cb7a2
ArtinTopojani <artintopojani2002@gmail.com>
2023-08-22 03:05:13 +0200

    ProductList & ProductCard

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,35 +1,31 @@
-import { useEffect, useState } from "react";
-import { Product } from "../models/product";
 import Catalog from "../../features/catalog/Catalog";
-import { Typography } from "@mui/material";
+import {
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
+import Header from "./Header";
+import { useState } from "react";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-
-  useEffect(() => {
-    fetch("http://localhost:5000/api/products")
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
-  }, []);
-
-  function addProduct() {
-    setProducts((prevState) => [
-      ...prevState,
-      {
-        id: prevState.length + 101,
-        name: "prod" + (prevState.length + 1),
-        price: prevState.length * 100 + 100,
-        brand: "brand",
-        description: "description ads",
-        // pictureUrl: "http://photo1.jpg",
-      },
-    ]);
-  }
+  // const [darkMode, setDarkMode] = useState(false);
+  // const paletteType = darkMode ? "dark" : "light";
+  // const theme = createTheme({
+  //   palette: {
+  //     mode: "dark",
+  //   },
+  // });
 
   return (
     <>
-      <Typography>BlueTech</Typography>
-      <Catalog products={products} addProduct={addProduct} />
+      {/* // <ThemeProvider theme={theme}> */}
+      <CssBaseline />
+      <Header />
+      <Container>
+        <Catalog />
+      </Container>
+      {/* // </ThemeProvider> */}
     </>
   );
 }
--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,5 +1,16 @@
-import { ListItem, ListItemAvatar, Avatar, ListItemText } from "@mui/material";
 import { Product } from "../../app/models/product";
+import { CardOverflow } from "@mui/joy";
+import * as React from "react";
+import AspectRatio from "@mui/joy/AspectRatio";
+import Button from "@mui/joy/Button";
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+import Chip from "@mui/joy/Chip";
+import Link from "@mui/joy/Link";
+import Typography from "@mui/joy/Typography";
+import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
+import { Elevator } from "@mui/icons-material";
+import { Box } from "@mui/material";
 
 interface Props {
   product: Product;
@@ -7,13 +18,53 @@ interface Props {
 
 export default function ProductCard({ product }: Props) {
   return (
-    <ListItem key={product.id}>
-      <ListItemAvatar>
-        <Avatar src={product.pictureUrl} />
-      </ListItemAvatar>
-      <ListItemText>
-        {product.name} - {product.price}
-      </ListItemText>
-    </ListItem>
+    <Card>
+      <CardOverflow>
+        <AspectRatio sx={{ minWidth: 200 }}>
+          <img
+            src={product.pictureUrl}
+            srcSet={product.pictureUrl}
+            loading="lazy"
+            alt="product photo"
+          />
+        </AspectRatio>
+      </CardOverflow>
+      <CardContent>
+        <Typography level="body-xs">
+          {product.type} / {product.brand}
+        </Typography>
+        <Link
+          href="#product-card"
+          fontWeight="md"
+          color="neutral"
+          textColor="text.primary"
+          overlay
+          // endDecorator={<ArrowOutwardIcon />}
+        >
+          {product.name}
+        </Link>
+
+        <Typography
+          level="title-lg"
+          sx={{ mt: 1, fontWeight: "xl" }}
+          // endDecorator={
+          //   <Chip component="span" size="sm" variant="soft" color="success">
+          //     Lowest price
+          //   </Chip>
+          // }
+        >
+          {(product.price / 100).toFixed(2)} €
+        </Typography>
+
+        <Typography level="body-sm">
+          (Only <b>{product.quantityInStock}</b> left in stock!)
+        </Typography>
+      </CardContent>
+      <CardOverflow>
+        <Button variant="solid" color="primary" size="lg">
+          Add to cart
+        </Button>
+      </CardOverflow>
+    </Card>
   );
 }

e36c5ae0f821e0bd54d7980049f5094d3b179a1b
ErmalKeqmezi <ekeqmezi10@gmail.com>
2023-08-28 13:52:34 +0200

    commit

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './app/layout/styles.css';
-
 import reportWebVitals from './reportWebVitals';
-import App from './app/layout/App';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/Router/Routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 

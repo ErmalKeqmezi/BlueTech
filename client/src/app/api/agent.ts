05485e8e589033f8e48296983f1b47a8d79a814d
ArtinTopojani <artintopojani2002@gmail.com>
2023-09-01 18:12:29 +0200

    loading component

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -2,62 +2,68 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { router } from "../Router/Routes";
 
-axios.defaults.baseURL = 'http://localhost:5000/api/';
+const sleep = () => new Promise((resolve) => setTimeout(resolve, 500));
+
+axios.defaults.baseURL = "http://localhost:5000/api/";
 
 const responseBody = (response: AxiosResponse) => response.data;
 
-axios.interceptors.response.use(response => {
+axios.interceptors.response.use(
+  async (response) => {
+    await sleep();
     return response;
-}, (error: AxiosError) => {
-    const {data, status} = error.response as AxiosResponse
-    switch(status) {
-        case 400: 
+  },
+  (error: AxiosError) => {
+    const { data, status } = error.response as AxiosResponse;
+    switch (status) {
+      case 400:
         if (data.errors) {
-            const modelStateErorr: string [] = [];
-            for (const key in data.errors) {
-                if(data.errors[key]) {
-                    modelStateErorr.push(data.errors[key])
-                }
+          const modelStateErorr: string[] = [];
+          for (const key in data.errors) {
+            if (data.errors[key]) {
+              modelStateErorr.push(data.errors[key]);
             }
-            throw modelStateErorr.flat();
-        } 
-        toast.error(data.title)
+          }
+          throw modelStateErorr.flat();
+        }
+        toast.error(data.title);
         break;
-        case 401: 
-        toast.error(data.title)
+      case 401:
+        toast.error(data.title);
         break;
-        case 500: 
-        router.navigate('/server-error', {state: {error: data}});
+      case 500:
+        router.navigate("/server-error", { state: { error: data } });
         break;
-        default:
+      default:
         break;
     }
-    return Promise.reject(error.response)
-})
+    return Promise.reject(error.response);
+  }
+);
 
 const requests = {
-    get: (url: string) => axios.get(url).then(responseBody),
-    post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
-    put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
-    delete: (url: string) => axios.delete(url).then(responseBody),
-}
+  get: (url: string) => axios.get(url).then(responseBody),
+  post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
+  put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
+  delete: (url: string) => axios.delete(url).then(responseBody),
+};
 
 const Catalog = {
-    list: () => requests.get('products'),
-    details: (id : number) => requests.get(`products/${id}`)
-}
+  list: () => requests.get("products"),
+  details: (id: number) => requests.get(`products/${id}`),
+};
 
 const ErrorTesting = {
-    get400Error: () => requests.get('bugs/bad-request'),
-    get401Error: () => requests.get('bugs/unauthorized'),
-    get404Error: () => requests.get('bugs/not-found'),
-    get500Error: () => requests.get('bugs/server-error'),
-    getValidationError: () => requests.get('bugs/validation-error'),
-}
+  get400Error: () => requests.get("bugs/bad-request"),
+  get401Error: () => requests.get("bugs/unauthorized"),
+  get404Error: () => requests.get("bugs/not-found"),
+  get500Error: () => requests.get("bugs/server-error"),
+  getValidationError: () => requests.get("bugs/validation-error"),
+};
 
 const agent = {
-    Catalog,
-    ErrorTesting
-}
+  Catalog,
+  ErrorTesting,
+};
 
 export default agent;
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -3,16 +3,24 @@ import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
 import React from "react";
 import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 
 export default function Catalog() {
   const [products, setProducts] = useState<Product[]>([]);
-  useEffect(() => {
-    agent.Catalog.list().then(products => setProducts(products))
-  }, [])
+  const [loading, setLoading] = useState(true);
 
-    return (
-       <>
-        <ProductList products={products} />
-      </>
-    )
+  useEffect(() => {
+    agent.Catalog.list()
+      .then((products) => setProducts(products))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return <LoadingComponent message="Loading products..." />;
+
+  return (
+    <>
+      <ProductList products={products} />
+    </>
+  );
 }

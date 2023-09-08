ca620df5c044882cad8e646f9d5a83dc5c88bbc8
ArtinTopojani <artintopojani2002@gmail.com>
2023-09-08 21:13:03 +0200

    Filters, pagination, search function

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { router } from "../Router/Routes";
+import { request } from "http";
 
 const sleep = () => new Promise((resolve) => setTimeout(resolve, 500));
 
@@ -43,15 +44,17 @@ axios.interceptors.response.use(
 );
 
 const requests = {
-  get: (url: string) => axios.get(url).then(responseBody),
+  get: (url: string, params?: URLSearchParams) =>
+    axios.get(url, { params }).then(responseBody),
   post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
   put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
   delete: (url: string) => axios.delete(url).then(responseBody),
 };
 
 const Catalog = {
-  list: () => requests.get("products"),
+  list: (params: URLSearchParams) => requests.get("products", params),
   details: (id: number) => requests.get(`products/${id}`),
+  fetchFilters: () => requests.get("products/filters"),
 };
 
 const Basket = {
--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -3,20 +3,18 @@ import { Product } from "../../app/models/product";
 import ProductCard from "./ProductCard";
 import React from "react";
 
-
 interface Props {
-    products: Product[];
+  products: Product[];
 }
 
-export default function ProductList({products} : Props) {
-    return (
-        <Grid container spacing={4}>
-        {products.map(product => (
-            <Grid item xs={3} key={product.id}> 
-            <ProductCard product={product}/> 
-            </Grid>
-        
-        ))}
-      </Grid>
-    )
+export default function ProductList({ products }: Props) {
+  return (
+    <Grid container spacing={4}>
+      {products.map((product) => (
+        <Grid item xs={4} key={product.id}>
+          <ProductCard product={product} />
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
